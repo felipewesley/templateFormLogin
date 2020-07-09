73021f74b9c8138a8b1e1728558f6d5eccf4f2a4
Felipe Wesley <fe.wesleybasso@gmail.com>
2020-07-09 00:36:31 -0300

    Validação dos dados fornecidos em inputs

--- validation.js
+++ validation.js
@@ -1,3 +1,18 @@
 $(document).ready(() => {
-    console.log('Hello world')
+    
+    $('#input-login').focus()
+
+    $('#btn-entrar').mousedown(e => {
+        $(e.target).css('background', '#3821ff')
+    }).mouseup(e => {
+        $(e.target).css('background', '#1300b9')
+    })
+
+    $('.input-value').keyup(e => {
+        if(String(e.target.value).indexOf(" ") != -1){
+            $(e.target).removeClass('primary').addClass('error')
+        } else {
+            $(e.target).removeClass('error').addClass('primary')
+        }
+    })
 })
